b465dc9a702d7593c0ced4d8743ac570b534911a
John Stockton <johnstocktoniv@gmail.com>
2025-08-11 22:39:47 -0400

    init

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM nginx:alpine
+RUN apk add --no-cache certbot certbot-nginx
+COPY --from=build /app/dist /usr/share/nginx/html
+COPY nginx.conf /etc/nginx/conf.d/default.conf
+COPY start-with-ssl.sh /start-with-ssl.sh
+RUN chmod +x /start-with-ssl.sh
+EXPOSE 443
+ENTRYPOINT ["/start-with-ssl.sh"]
---
+++ start-with-ssl.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Check if certificates exist, if not, run the init script
+if [ ! -d "/etc/letsencrypt/live" ]; then
+    domain="johncannothearyou.com"
+    email="johncannothearyou@gmail.com"
+
+    # Stop nginx if it's running
+    nginx -s stop || echo "Nginx is not running, proceeding with certificate generation."
+
+    # Get the certificate
+    certbot certonly --standalone \
+        --agree-tos \
+        --non-interactive \
+        --email $email \
+        --domain $domain \
+        --domain www.$domain
+else 
+    certbot renew
+fi
+
+# Add a cron job to renew the certificate
+echo "0 12 * * * certbot renew --quiet" | crontab -
+
+# Start Nginx
+nginx -g "daemon off;"
